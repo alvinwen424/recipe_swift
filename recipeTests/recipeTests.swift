30753a0a826d7b6d8c2fe5d830641e5e831b11e9
Alvin Wen <alvinwen424@gmail.com>
2018-06-30 21:38:01 -0400

    Initial Commit

---
+++ recipeTests/recipeTests.swift
@@ -0,0 +1,36 @@
+//
+//  recipeTests.swift
+//  recipeTests
+//
+//  Created by alvin wen on 6/30/18.
+//  Copyright © 2018 alvin wen. All rights reserved.
+//
+
+import XCTest
+@testable import recipe
+
+class recipeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
